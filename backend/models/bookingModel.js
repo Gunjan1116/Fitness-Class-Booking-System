1edc24b1d010baaa772dc970adb61fcea3e0c8b3
Gunjan1116 <kumargunjan1116@gmail.com>
2023-03-29 22:28:49 +0530

    day-2

---
+++ backend/models/bookingModel.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+
+const bookingSchema=mongoose.Schema({
+    userId:{type:String,require:true},
+    trainerId:{type:String,require:true},
+    bookingDate:{type:String,require:true},//mm/dd/yyyy
+    bookingSlot:{type:String,require:true}
+},{timestamps:true})
+
+const Bookingmodel=mongoose.model("booking",bookingSchema)
+
+module.exports={Bookingmodel}
+// let x=new Date();
+
+// console.log(x);
+
+// // console.log(x.getUTCFullYear());
+// //console.log(x.getUTCMonth());
+// // console.log(x.getUTCDate());
+// // console.log(x.getUTCHours());
+// // console.log(x.getUTCMinutes());
+// // console.log(x.getUTCSeconds());
+// console.log(x.toLocaleDateString())
