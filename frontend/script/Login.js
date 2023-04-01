9a6eace583cfba179bdca598cabed37a9e4fd270
Gunjan1116 <kumargunjan1116@gmail.com>
2023-04-02 01:08:58 +0530

    added appointment js

--- frontend/script/Login.js
+++ frontend/script/Login.js
@@ -1,4 +1,4 @@
-let baseUrl=`http://localhost:5000`
+let baseUrl=`http://localhost:3000`
 
 
 let loginForm = document.querySelector('.login-wrap');
---
+++ frontend/script/appointment.js
@@ -0,0 +1,49 @@
+let baseUrl=`http://localhost:3000`
+let btnBook=document.getElementById("book_appointment");
+btnBook.addEventListener("click",()=>{
+    let date=document.getElementById("inputdate").value;
+    let slot=document.getElementById("slot-select").value;
+    let token=sessionStorage.getItem("token");
+    let name=sessionStorage.getItem("name");
+    let trainerId=sessionStorage.getItem("trainerId");
+    if(!token){
+        alert("Please Login First to Book an Appointment!!")
+        window.location.href="./Login.html"
+    }else if(date==""||slot==""){
+        alert("Please fill all the fields")
+    }else{
+       let obj={
+        trainerId:trainerId,
+        bookingDate:date,
+        bookingSlot:slot
+       }
+       bookAnAppointment(obj,token,name);
+    }
+    
+})
+
+async function bookAnAppointment(obj,token,name){
+
+    try {
+        let res=await fetch(`${baseUrl}/booking/create`,{
+         method:'POST',
+         headers:{
+             'Content-type':'application/json',
+             Authorization:`${token}`
+         },
+         body:JSON.stringify(obj)
+        })
+        let out=await res.json();
+        //console.log(out);
+        if(out.msg=="This Slot is Not Available."){
+         alert("This Slot is Not Available.")
+        }else if(out.msg=="new booking created successfully"){
+         alert(`Hii ${name} Your booking is confirmed on ${obj.bookingDate}`)
+        }else{
+         alert("Something went wrong!!")
+        }
+     } catch (error) {
+         console.log("err",error)
+         alert("Something went wrong!!!!")
+     }
+}
