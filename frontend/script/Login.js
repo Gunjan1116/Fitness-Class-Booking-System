323fb598bbec42039ba073724f89b0678074bdb2
Gunjan Kumar <112754725+Gunjan1116@users.noreply.github.com>
2023-06-22 15:57:53 +0530

    Update admin login in Login.js

--- frontend/script/Login.js
+++ frontend/script/Login.js
@@ -70,9 +70,11 @@ loginBtn.addEventListener("click",(e)=>{
     let password=document.querySelector(".login-wrap form #password").value;
     if(email==""||password==""){
         return alert("Please fill all the details!")
-    }else if(email=="admin@gmail.com"&&password=="admin"){
-        window.location.href="./adminpage.html";
-    }else{
+    }
+    // else if(){
+    //     window.location.href="./adminpage.html";
+    // }
+    else{
         let obj={
             email,
             password
